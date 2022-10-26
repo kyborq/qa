28a33de3ca6c819fd82d5cde36a4576b48681437
Konstantin <k.podyganov@mail.ru>
2022-10-27 01:18:29 +0300

    Init lab 2

---
+++ lab2/CheckURL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckURL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
